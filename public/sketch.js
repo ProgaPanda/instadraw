064ba243cf2a026e4e84f9fbaf74f7b56d6d6299
Khaled Alba <looda97@gmail.com>
2018-11-15 18:36:18 +0300

    v1.04

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,13 @@
 let socket;
+function loadImgErrFix(errEvt) {
+  const pic = errEvt.target;
 
+  if (!pic.crossOrigin)  return p.print(`Failed to reload ${pic.src}!`);
+
+  p.print(`Attempting to reload ${pic.src} as a tainted image now...`);
+  pic.crossOrigin = null, pic.src = pic.src;
+}
+});
 function setup() {
   createCanvas(400, 480);
   background(51);
@@ -17,7 +25,7 @@ function setup() {
       console.log(data);
       let image = document.createElement("img");
       image.src = data.url;
-    });
+    }, loadImgErrFix);
   });
 }
 
