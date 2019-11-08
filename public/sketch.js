61795fa52a66b495509f023df9c5dd21765f3b55
Khaled Albarawy <looda97@gmail.com>
2019-11-08 10:46:38 +0200

    [REFACTOR] refactoring the client file

--- public/sketch.js
+++ public/sketch.js
@@ -1,55 +1,58 @@
+// Initial values
 let socket;
 let data;
 let points = [];
 let font;
-let current_color;
-let color_picking = false;
+let currentColor;
+let isPickingColor = false;
 let isErasing = false;
-let STROKE_WEIGHT = 4;
+let strokeWeight = 4;
+
 function preload() {
-  font = loadFont("css/fonts/DancingScript-Bold.ttf");
+  font = loadFont('css/fonts/DancingScript-Bold.ttf');
 }
 
 function setup() {
   let canvas = createCanvas(window.innerWidth, window.innerHeight);
-  canvas.parent("canvas");
+  canvas.parent('canvas');
   window.scrollTo(0, 1);
 
+  // Setting background color in the canvas
   background(51);
 
-  var colorPicker = $(".colorPickSelector").colorPick({
-    initialColor: "#ecf0f1",
+  $('.colorPickSelector').colorPick({
+    initialColor: '#ecf0f1',
     allowRecent: false,
-    returnColor: "#000",
+    returnColor: '#000',
     palette: [
-      "#ecf0f1",
-      "#f1c40f",
-      "#1abc9c",
-      "#2980b9",
-      "#9b59b6",
-      "#34495e",
-      "#2c3e50",
-      "#c0392b",
-      "#bdc3c7"
+      '#ecf0f1',
+      '#f1c40f',
+      '#1abc9c',
+      '#2980b9',
+      '#9b59b6',
+      '#34495e',
+      '#2c3e50',
+      '#c0392b',
+      '#bdc3c7',
     ],
     onColorSelected: function() {
       this.element.css({ backgroundColor: this.color, color: this.color });
-      current_color = this.color;
+      currentColor = this.color;
       returnColor = this.color;
-    }
+    },
   });
 
   //check if in dev mode
   hostname =
-    window.location.hostname == "localhost"
-      ? "http://localhost:3000"
+    window.location.hostname == 'localhost'
+      ? 'http://localhost:3000'
       : window.location.hostname;
 
   //socket client instance
   socket = io.connect(hostname);
 
   //load current drawing for the newly connected client
-  socket.on("stored_drawing", data => {
+  socket.on('stored_drawing', data => {
     beginShape();
     strokeWeight(4);
     noFill();
@@ -59,6 +62,9 @@ function setup() {
       let y = data[i].y;
       stroke(data[i].color);
       vertex(x, y);
+
+      /* If this pixel is the beginning of a new line
+       break the line and start a new one */
       if (data[i].newLine) {
         endShape();
         beginShape();
@@ -68,7 +74,7 @@ function setup() {
 
   //setup a listener for broadcast
   let points = [];
-  socket.on("broadcast", data => {
+  socket.on('broadcast', data => {
     points.push(createVector(data.x, data.y));
 
     stroke(data.color);
@@ -87,31 +93,29 @@ function setup() {
     endShape();
   });
 
-  $(".eraser-button").on("click", function() {
-    $(this).toggleClass("eraser-button-activated");
+  $('.eraser-button').on('click', function() {
+    $(this).toggleClass('eraser-button-activated');
     //toggle between eraser and current color
     if (!isErasing) {
-      current_color = "#333333";
+      currentColor = '#333333';
       isErasing = true;
-      STROKE_WEIGHT = 16;
+      strokeWeight = 16;
     } else {
-      current_color = returnColor;
+      currentColor = returnColor;
       isErasing = false;
-      STROKE_WEIGHT = 4;
+      strokeWeight = 4;
     }
   });
 
-  // socket.on("change_STROKE_WEIGHT", weight => {
-  //   STROKE_WEIGHT = weight;
-  // });
-
-  $(".clear-button").click(() => {
-    socket.emit("clear");
+  $('.clear-button').click(() => {
+    socket.emit('clear');
   });
-  socket.on("clear-client", () => {
+
+  socket.on('clear-client', () => {
     background(51);
   });
-  $(".save-button").click(() => {
+
+  $('.save-button').click(() => {
     const id = Math.floor(random(13081997));
     save(`instadraw-${id}.png`);
   });
@@ -121,15 +125,15 @@ function draw() {
   data = {
     x: mouseX,
     y: mouseY,
-    color: current_color,
+    color: currentColor,
     newLine: false,
-    strokeWeight: STROKE_WEIGHT
+    strokeWeight: strokeWeight,
   };
 
-  if (mouseIsPressed && !color_picking) {
+  if (mouseIsPressed && !isPickingColor) {
     points.push(createVector(mouseX, mouseY));
-    stroke(current_color);
-    strokeWeight(STROKE_WEIGHT);
+    stroke(currentColor);
+    strokeWeight(strokeWeight);
     noFill();
     beginShape();
     for (let i = 0; i < points.length; i++) {
@@ -140,17 +144,17 @@ function draw() {
     endShape();
 
     //send the cordenator data
-    socket.emit("cords", data);
+    socket.emit('cords', data);
   }
 
-  socket.on("clients_counter", counter => {
-    let users_text = document.querySelector(".online-users");
+  socket.on('clients_counter', counter => {
+    let users_text = $('.online-users');
     users_text.innerHTML = `<i class="fas fa-users"></i> ${counter}`;
   });
 }
 
 function mouseReleased() {
   data.newLine = true;
-  socket.emit("cords", data);
+  socket.emit('cords', data);
   points = [];
 }
